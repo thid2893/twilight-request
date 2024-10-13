6cb700729564a08f8b16015005e65b8cbd3faf64
thid2893 <thid2893@outlook.com>
2024-10-13 11:54:11 +0700

    .

---
+++ logger.js
@@ -0,0 +1,8 @@
+exports.logger = function (fn) {
+  return function (...args) {
+    console.time(`Execution time for ${fn.name}`);
+    const result = fn.apply(this, args);
+    console.timeEnd(`Execution time for ${fn.name}`);
+    return result;
+  };
+};
